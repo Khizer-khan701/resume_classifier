aceb9a00b46d91ea93c4cc38684d3520e763cc6a
smkhizarai025 <smkhizarai025@gmail.com>
2026-03-11 01:44:05 +0500

    Ultra-Minimalist Diagnostic: Stripped all middleware and headers

--- resume_classifier_project/settings/prod.py
+++ resume_classifier_project/settings/prod.py
@@ -3,38 +3,26 @@ import os
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
-# Override MIDDLEWARE to remove SecurityMiddleware (most common cause of loops)
+# Disable all middleware that could cause redirects
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'common.middleware.GlobalErrorHandlingMiddleware',
 ]
 
-# Hard-fix for Hugging Face Proxy
+ALLOWED_HOSTS = ['*']
+
+# Absolute minimum proxy settings
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
-USE_X_FORWARDED_PORT = True
 
-# Disable all internal redirects
+# Explicitly disable all redirect-prone features
 SECURE_SSL_REDIRECT = False
 APPEND_SLASH = False
 SECURE_HSTS_SECONDS = 0
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_HSTS_PRELOAD = False
 
-# Cookie settings for iframes
-SESSION_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
-CSRF_COOKIE_SECURE = True
-
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
