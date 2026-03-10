d429239d3c6f4f241faf30ec893747ff22059b75
smkhizarai025 <smkhizarai025@gmail.com>
2026-03-11 01:29:11 +0500

    Aggressive fix for redirect loop: Removed SecurityMiddleware and forced proxy settings

--- resume_classifier_project/settings/prod.py
+++ resume_classifier_project/settings/prod.py
@@ -1,24 +1,43 @@
 from .base import *
 import os
 
-DEBUG = True  # Temporarily enabled to see the actual error
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# We are using sqlite3 for basic Django requirements
+# Override MIDDLEWARE to remove SecurityMiddleware (most common cause of loops)
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'common.middleware.GlobalErrorHandlingMiddleware',
+]
+
+# Hard-fix for Hugging Face Proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
+# Disable all internal redirects
+SECURE_SSL_REDIRECT = False
+APPEND_SLASH = False
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+
+# Cookie settings for iframes
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Simplified Security (Disabled for debugging)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-USE_X_FORWARDED_HOST = True
-SECURE_SSL_REDIRECT = False
-SECURE_HSTS_SECONDS = 0
-SECURE_CONTENT_TYPE_NOSNIFF = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-X_FRAME_OPTIONS = 'ALLOWALL' # Help with HF iframe
