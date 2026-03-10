fd02d3e8fdda0d80ed41fc3a3ed720766dc3bdbb
smkhizarai025 <smkhizarai025@gmail.com>
2026-03-11 01:27:15 +0500

    Enable DEBUG and simplify settings for HF connectivity debugging

--- resume_classifier_project/settings/prod.py
+++ resume_classifier_project/settings/prod.py
@@ -1,11 +1,11 @@
 from .base import *
+import os
 
-DEBUG = False
+DEBUG = True  # Temporarily enabled to see the actual error
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
-# We are using sqlite3 for basic Django requirements (Admin, Sessions, ContentTypes)
-# App-specific data is stored in Firestore.
+# We are using sqlite3 for basic Django requirements
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -13,15 +13,12 @@ DATABASES = {
     }
 }
 
-# Production Security Headers
+# Simplified Security (Disabled for debugging)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
 SECURE_SSL_REDIRECT = False
-SECURE_HSTS_SECONDS = 0  # Disable HSTS to prevent browser-side redirect loops during debug
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_HSTS_PRELOAD = False
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 0
+SECURE_CONTENT_TYPE_NOSNIFF = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+X_FRAME_OPTIONS = 'ALLOWALL' # Help with HF iframe
